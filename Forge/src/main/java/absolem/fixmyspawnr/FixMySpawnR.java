9ee6e4541219d43c64bbcb5e7e6a661373dbca01
Subaraki <robbeenaxel@hotmail.com>
2022-05-21 21:07:46 +0200

    server sided addition for forge

--- Forge/src/main/java/absolem/fixmyspawnr/FixMySpawnR.java
+++ Forge/src/main/java/absolem/fixmyspawnr/FixMySpawnR.java
@@ -1,15 +1,20 @@
 package absolem.fixmyspawnr;
 
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod("fixmyspawnr")
 public class FixMySpawnR {
 
     public FixMySpawnR() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+                () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (ver, remote) -> true));
+
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::modConfig);
         ModLoadingContext modLoadingContext = ModLoadingContext.get();
         modLoadingContext.registerConfig(ModConfig.Type.SERVER, ConfigData.SERVER_SPEC);
